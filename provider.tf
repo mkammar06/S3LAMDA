cdd1b592da786ba9420ddff6e2dce4bc2c27bed2
mkammar <manjunath.kammar@epsilonconversant.com>
2021-04-28 12:53:32 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+# terraform {
+#   backend "s3"{
+#       bucket ="S3LambdaBucketManju"
+#       key = "mkammar06/tfstate"
+#       region = "us-east-1"
+#   }
+# }
